a7a9f7cd3d0de0ff493b0c52909a3420d963106a
Sandile-Memela <sah.memela@gmail.com>
2024-05-29 17:42:00 +0200

    fix: updated to using json parse for body

--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@ import { generateShortCode, throwIfMissing } from "./utils.js";
 export default async (context) => {
   try {
     throwIfMissing(process.env, ["LIVEKIT_API_KEY", "LIVEKIT_API_SECRET"]);
+    // Parse JSON body
+    const body = JSON.parse(context.req.body);
     // if this room doesn't exist, it'll be automatically created when the first
     // client joins
-    const roomName = context.req.body.roomName;
+    const roomName = context.req.body.roomName || body.roomName;
     // identifier to be used for participant.
     // it's available as LocalParticipant.identity with livekit-client SDK
-    const participantName = context.req.body.participantName;
+    const participantName =
+      context.req.body.participantName || body.participantName;
     const at = new AccessToken(
       process.env.LIVEKIT_API_KEY,
       process.env.LIVEKIT_API_SECRET,
@@ -20,9 +23,6 @@ export default async (context) => {
       }
     );
     at.addGrant({ roomJoin: true, room: roomName });
-    context.log(context.req.body);
-    context.log(context.req.body.roomName);
-    context.log(JSON.parse(context.req.body).roomName);
     return context.res.json({ ok: true, token: await at.toJwt() });
   } catch ({ name, message }) {
     return context.res.json(
