f1ea3401c767a6e4026fa21b4bf72cc0ae336e1c
Sandile-Memela <sah.memela@gmail.com>
2024-05-29 17:24:50 +0200

    fix: updated logging for req body

--- src/main.js
+++ src/main.js
@@ -20,6 +20,7 @@ export default async (context) => {
       }
     );
     at.addGrant({ roomJoin: true, room: roomName });
+    context.log(context.req.body);
     return context.res.json({ ok: true, token: await at.toJwt() });
   } catch ({ name, message }) {
     return context.res.json(
